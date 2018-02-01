acd5faaaeb07d8b2b33ca679ef8aeb29710fac38
Josh Long <starbuxman@gmail.com>
2018-02-01 03:36:12 -0800

    fixed typo

--- producer-binder-kafka/src/test/java/com/example/producerbinderkafka/ProducerBinderKafkaApplicationTests.java
+++ producer-binder-kafka/src/test/java/com/example/producerbinderkafka/ProducerBinderKafkaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.producerbinderkakfa;
+package com.example.producerbinderkafka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProducerBinderKakfaApplicationTests {
+public class ProducerBinderKafkaApplicationTests {
 
 	@Test
 	public void contextLoads() {
